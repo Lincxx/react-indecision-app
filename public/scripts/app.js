76620c098e56962e1d249d56b290c7a1fc1bb994
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2018-03-16 10:54:36 -0400

    add in bable, react, reat-dom, env

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,13 @@
-console.log('App is running')
+'use strict';
+
+console.log('App is running');
 
 // JSX - JavaScript XML
-var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
 var appRoot = document.querySelector('#app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App is running')
+
+// JSX - JavaScript XML
+var template = <h1>Indecision App</h1>;
+var appRoot = document.querySelector('#app');
+
+ReactDOM.render(template, appRoot);
